0036ea0c6b47d6cd8c6919bb99815db03927cb5e
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2023-09-20 00:10:19 +0530

    Day 1.c

--- Day 1.c
+++ Day 1.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
-int main () {
-    printf("Hello, World!");
+// Function to calculate factorial of a number
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int t;
+    printf("Enter the number of test cases (1 <= t <= 100): ");
+    scanf("%d", &t);
+
+    // Loop through each test case
+    while (t--) {
+        int n;
+        printf("Enter an integer n (1 <= n <= 100) for the test case: ");
+        scanf("%d", &n);
+
+        // Calculate the factorial of n
+        unsigned long long result = factorial(n);
+
+        // Display the result
+        printf("%llu\n", result);
+    }
 
     return 0;
-
 }
